b61d522cd0639959164a0f8f13c40d27ae5f792b
cherryApp <j.cserko@gmail.com>
2024-05-24 11:08:51 +0200

    feat: using env vars

---
+++ nx-env/apps/nx-env/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NxWelcomeComponent } from './nx-welcome.component';
+
+@Component({
+  standalone: true,
+  imports: [NxWelcomeComponent, RouterModule],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'nx-env';
+
+  constructor() {
+    console.log('>>> NX_API_URL', process.env['NX_API_URL']);
+  }
+}
